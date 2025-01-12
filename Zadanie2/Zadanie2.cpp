7f83d3ca77ccb206a79fab5586f1324bdf8bdd35
Danil Zholobov <asusmemp10@gmail.com>
2025-01-12 23:32:43 +0300

    Не смог сделатьконструктор из наследника нормальный. Постоянно вылазит ошибка

--- Zadanie2/Zadanie2.cpp
+++ Zadanie2/Zadanie2.cpp
@@ -2,12 +2,13 @@
 #include <iostream>
 #include <locale>
 #include <cstring>
+#include <conio.h>
 
 using namespace std;
 
 class String
 {
-private:
+protected:
     enum { SZ = 80 };        
     char str[SZ];         
 public:
@@ -28,19 +29,41 @@ public:
     {
         return str;
     }
+    //virtual ~String() {
+     //   delete[] str; // Освобождаем выделенную память
+    //    cout << "Base destructor called" << endl;
+   // }
 };
 
+class Pstring : public String {
+public:
+    Pstring(const char s[]);
+};
+Pstring::Pstring(const char s[]) {
+    if (strlen(s) > SZ - 1) {
+        for (int i = 0; i < SZ - 1; i++) {
+            str[i] = s[i];
+        }
+        str[SZ - 1] = '\x0';
+    }
+    else strcpy(str, s);
+}
 /////////////////////////////////////////////////////////// 
 int main()
 {
     setlocale(LC_ALL, "Russian");
-    String s1;                 
-    char xstr[] = "Ура, товарищи! "; 
-    s1 = xstr;              
-    s1.display();   
-    String s2 = "Мы победим! "; 
-    cout << static_cast<char*>(s2);
-    cout << xstr << endl;
+    //String s1;                 
+    //char xstr[] = "Ура, товарищи! "; 
+    //s1 = xstr;              
+    //s1.display();   
+    //String s2 = "Мы победим! ";
+    //cout << static_cast<char*>(s2);
+    //cout << xstr << endl;
+    Pstring A("Я рисую, я Ван Гог ");
+    Pstring B("Эта строка имеет очень большую длину и мы можем быть уверены, что она не уместится в отведенный буфер, что приведет к непредсказуемым последствиям.");
+    _getch();
+    cout << static_cast<char*>(A) << static_cast<char*>(B);
+    _getch();
     return 0;
 }
 
